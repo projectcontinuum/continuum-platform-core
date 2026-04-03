9d54d5fdce28fe7f8c993b52787e4f3f7efe7429
roushan65 <roushan65@gmail.com>
2026-04-03 18:27:14 +0530

    CONTNM-81: added Landing page module

---
+++ landing-page/build.gradle.kts
@@ -0,0 +1,54 @@
+import com.github.gradle.node.npm.task.NpmTask
+
+plugins {
+    id("com.github.node-gradle.node") version "3.2.1"
+}
+
+// Read version from package.json
+val packageJsonFile = file("package.json")
+val packageJson = groovy.json.JsonSlurper().parseText(packageJsonFile.readText()) as Map<*, *>
+version = packageJson["version"] ?: throw GradleException("Unable to read version from package.json")
+
+node {
+    version.set("22.12.0") // Specify the Node.js version
+    download.set(true) // Automatically download and install Node.js
+}
+
+
+tasks.register<NpmTask>("build") {
+    dependsOn("npmInstall")
+    args.set(listOf("run", "build"))
+}
+
+tasks.register<Delete>("clean") {
+    delete("dist")
+}
+
+tasks.named("build") {
+    dependsOn("clean")
+}
+
+tasks.register<NpmTask>("run") {
+    args.set(listOf("run", "dev"))
+}
+
+tasks.register("publish") {
+    description = "Publish the built application to JFrog Artifactory"
+    group = "Publishing tasks"
+    // don't publish yet
+}
+
+tasks.register<Exec>("jib") {
+    description = "Docker build and push to Docker Hub"
+    group = "Jib tasks"
+    val dockerRepoName = "docker.io/${(System.getenv("GITHUB_REPOSITORY") ?: property("repoName").toString()).lowercase()}"
+    val imageName = "$dockerRepoName:${project.version}"
+    val latestImageName = "$dockerRepoName:latest"
+    commandLine("bash", "-c",
+        "docker build -t $imageName -t $latestImageName . --progress=plain && " +
+            "docker login docker.io --username ${System.getenv("DOCKER_REPO_USERNAME")} --password ${System.getenv("DOCKER_REPO_PASSWORD")} && " +
+            "docker push $imageName && " +
+            "docker push $latestImageName"
+    )
+}
+
